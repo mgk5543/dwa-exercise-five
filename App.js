8356552e42a9431e0149d313e5427b1bedeb08f5
mgk5543 <mgk5543@nyu.edu>
2022-11-03 09:25:57 -0400

    port changes

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 const indexRoute = require('./routes/index.js')
 
 app.use('/', indexRoute)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Working!`)
 })
