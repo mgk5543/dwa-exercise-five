42127d111c455aa4de9931ee5d5ad3bfdea6e510
mgk5543 <mgk5543@nyu.edu>
2022-11-03 09:12:56 -0400

    Add simple server

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+
+//middleware specific to this router
+router.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+})
+
+//define the index route
+router.get('/', (req, res) => {
+    res.send('Wow!')
+})
+
+module.exports = router
